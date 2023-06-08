0243b10f1fd829bc5773f8329d8c7ff18d544cd4
mohammadmustaqhpt123 <99659714+mohammadmustaqhpt123@users.noreply.github.com>
2023-06-08 17:38:36 +0530

    Create How to handle Hiding Element in Selenium.js

---
+++ How to handle Hiding Element in Selenium.js
@@ -0,0 +1,18 @@
+var fulldoc='';
+var scrollX=0;
+var scrollY=0;
+
+document.addEventListener("keydown",function(event){
+    if(event.key=='q' && event.altKey){
+        fulldoc=document.body.outerHTML;
+        scrollY=window.pageYOffset;
+        scrollX=window.pageXOffset;
+    }
+    if(event.key=='w' && event.altKey){
+        document.body.outerHTML=fulldoc;
+        document.body.scrollTo(scrollX,scrollY);
+    }
+});
+
+
+When the window looks the way you would want to scan, press 'Alt + Q', then press 'Alt + W'. Now your window will freeze and then you can scan your page. To steer the objects you need to refresh your browser.
